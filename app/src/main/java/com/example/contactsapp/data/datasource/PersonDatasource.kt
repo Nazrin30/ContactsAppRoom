c3a136d32f93a0245ca4b0c496c82c28b5a6b94a
Nazrin30 <nezrin.mamedova@yahoo.com>
2023-02-15 18:39:40 +0400

    Initial commit

---
+++ app/src/main/java/com/example/contactsapp/data/datasource/PersonDatasource.kt
@@ -0,0 +1,35 @@
+package com.example.contactsapp.data.datasource
+
+import android.util.Log
+import com.example.contactsapp.data.entity.Persons
+import com.example.contactsapp.room.PersonsDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class PersonDatasource(var pdao:PersonsDao) {
+
+    suspend fun save(name:String,phone:String){
+        val newPerson = Persons(0,name,phone)
+        pdao.save(newPerson)
+    }
+
+    suspend fun update(id:Int,name:String,phone:String){
+        val updatePerson = Persons(id,name,phone)
+        pdao.update(updatePerson)
+    }
+
+    suspend fun delete(id:Int){
+        val deletePerson = Persons(id,"","")
+        pdao.delete(deletePerson)
+    }
+
+    suspend fun loadPersons() : List<Persons>  =
+        withContext(Dispatchers.IO){
+            pdao.loadPersons()
+        }
+
+    suspend fun search(searchText:String) : List<Persons>  =
+        withContext(Dispatchers.IO){
+            pdao.search(searchText)
+        }
+}
